48fab85ecf09cefe689ba9b7cb99aa778647f161
Sammo <sammo.wong@foxmail.com>
2019-08-11 16:44:22 +0800

    add extensions

---
+++ src/Growth/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Growth.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 日期格式化为字符串，格式"yyyy-MM-dd"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd");
+        }
+
+        /// <summary>
+        /// 日期格式化为字符串，格式"yyyy-MM-dd"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+
+            return dateTime.ToDateString();
+        }
+
+        /// <summary>
+        /// 日期格式化为字符串，格式"yyyy-MM-dd HH:mm:ss"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToDateTimeString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        /// <summary>
+        /// 日期格式化为字符串，格式"yyyy-MM-dd HH:mm:ss"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToDateTimeString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+
+            return dateTime.ToDateTimeString();
+        }
+
+        /// <summary>
+        /// 日期格式化为字符串，格式"HH:mm:ss"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToTimeString(this DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm:ss");
+        }
+
+        /// <summary>
+        /// 日期格式化为字符串，格式"HH:mm:ss"
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToTimeString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+
+            return dateTime.ToTimeString();
+        }
+    }
+}
---
+++ src/Growth/Extensions/QueryableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Growth.Extensions
+{
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// 添加查询条件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, bool condition)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
