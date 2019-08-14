476b5b7a1ec1ad869cb578aebe4769f44bc98805
Sammo <sammo.wong@foxmail.com>
2019-08-14 17:24:58 +0800

    add CollectionExtensions

---
+++ src/Growth/Extensions/CollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Growth.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static T OneOf<T>(this ICollection<T> source)
+        {
+            var count = source.Count;
+            return source.ToList()[new Random().Next(0, count)];
+        }
+
+        public static bool AddIf<T>(this ICollection<T> source, Func<T, bool> predicate, T value)
+        {
+            if (predicate(value))
+            {
+                source.Add(value);
+                return true;
+            }
+            return false;
+        }
+
+        public static void RemoveIf<T>(this ICollection<T> source, Func<T, bool> predicate, T value)
+        {
+            if (predicate(value))
+            {
+                source.Remove(value);
+            }
+        }
+
+        public static void RemoveWhere<T>(this ICollection<T> source, Func<T, bool> predicate)
+        {
+            var list = source.Where(predicate);
+            foreach (var item in list)
+            {
+                source.Remove(item);
+            }
+        }
+    }
+}
--- src/Growth/Extensions/QueryableExtensions.cs
+++ src/Growth/Extensions/QueryableExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace Growth.Extensions
 {
@@ -22,6 +21,44 @@ namespace Growth.Extensions
                 throw new ArgumentNullException(nameof(source));
 
             return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="skipCount"></param>
+        /// <param name="takeCount"></param>
+        /// <returns></returns>
+        public static IQueryable<T> Page<T>(this IQueryable<T> source, int skipCount, int takeCount)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.Skip(skipCount).Take(takeCount);
+        }
+
+        /// <summary>
+        /// 排序并分页
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="skipCount"></param>
+        /// <param name="takeCount"></param>
+        /// <param name="ordering">排序规则：Id DESC</param>
+        /// <returns></returns>
+        public static IQueryable<T> Page<T>(this IQueryable<T> source, int skipCount, int takeCount, string ordering)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (!ordering.IsNullOrWhiteSpace())
+            {
+                var orderedSource = source as IOrderedQueryable<T>;
+                return orderedSource.OrderBy(ordering).Skip(skipCount).Take(takeCount);
+            }
+            return source.Skip(skipCount).Take(takeCount);
         }
     }
 }
