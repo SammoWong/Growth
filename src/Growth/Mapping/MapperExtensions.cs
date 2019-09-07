572d3b9634e454e83455b16b1e6e21f5d24b5ce4
Sammo <sammo.wong@foxmail.com>
2019-09-07 15:00:25 +0800

    addMapperExtensions

---
+++ src/Growth/Mapping/MapperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Growth.Mapping
+{
+    public static class MapperExtensions
+    {
+        public static TTarget MapTo<TSource, TTarget>(this TSource source, TTarget target)
+        {
+            throw new Exception();
+        }
+
+        public static TTarget MapTo<TSource, TTarget>(this TSource source)
+        {
+            throw new Exception();
+        }
+    }
+}
